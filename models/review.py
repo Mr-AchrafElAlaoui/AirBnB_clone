6a85b12a0c11703368fcacf721b9c371f3559708
Mr.ElAlaoui <hraf23@gmail.com>
2024-05-20 12:50:19 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Documenting the user model"""
+"""Module for Review class."""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """The review class"""
+    """Class representing a Review."""
     place_id = ""
     user_id = ""
     text = ""
